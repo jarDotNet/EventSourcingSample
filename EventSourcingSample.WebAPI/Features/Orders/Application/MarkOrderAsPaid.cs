3cb2ec6d4ec7a666d164ce5f18141f07e9ade442
jardotnet <jarDotNet@users.noreply.github.com>
2026-02-02 11:41:49 +0100

    Apply ROP to order state use cases

--- EventSourcingSample.WebAPI/Features/Orders/Application/MarkOrderAsPaid.cs
+++ EventSourcingSample.WebAPI/Features/Orders/Application/MarkOrderAsPaid.cs
@@ -22,18 +22,35 @@ public sealed class MarkOrderAsPaid
 
         public async Task<Result<Unit>> Handle(Guid orderId, CancellationToken cancellationToken = default)
         {
+            return await GetOrderDetails(orderId, cancellationToken)
+                .Bind(ApplyOrderPaidEvent)
+                .Bind(x => SaveOrder(x, cancellationToken));
+        }
+
+        private async Task<Result<OrderDetails>> GetOrderDetails(Guid orderId, CancellationToken cancellationToken = default)
+        {
             var orderDetails = await _orderRepository.GetById(orderId, cancellationToken);
             if (orderDetails is null)
             {
-                return Result.NotFound($"Order {orderId} not found");
+                return Result.NotFound<OrderDetails>($"Order {orderId} not found");
             }
             if (orderDetails.Status != OrderStatus.Created)
             {
-                return Result.Failure($"Order {orderId} with status {orderDetails.Status} cannot be set as Paid");
+                return Result.Failure<OrderDetails>($"Order {orderId} with status {orderDetails.Status} cannot be set as Paid");
             }
 
+            return orderDetails;
+        }
+
+        private static Result<OrderDetails> ApplyOrderPaidEvent(OrderDetails orderDetails)
+        {
             orderDetails.Apply(new OrderPaid());
 
+            return orderDetails;
+        }
+
+        private async Task<Result<Unit>> SaveOrder(OrderDetails orderDetails, CancellationToken cancellationToken = default)
+        {
             await _orderRepository.Save(orderDetails, cancellationToken);
 
             return Result.Success();
