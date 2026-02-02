7255693c41c182656ab5a439936351f7fd125045
jardotnet <jarDotNet@users.noreply.github.com>
2026-02-02 11:41:49 +0100

    Apply ROP toorder state use cases

--- EventSourcingSample.WebAPI/Features/Orders/Application/MarkOrderAsDelivered.cs
+++ EventSourcingSample.WebAPI/Features/Orders/Application/MarkOrderAsDelivered.cs
@@ -22,18 +22,35 @@ public sealed class MarkOrderAsDelivered
 
         public async Task<Result<Unit>> Handle(Guid orderId, CancellationToken cancellationToken = default)
         {
+            return await GetOrderDetails(orderId, cancellationToken)
+                .Bind(ApplyOrderCompletedEvent)
+                .Bind(x => SaveOrder(x, cancellationToken));
+        }
+
+        private async Task<Result<OrderDetails>> GetOrderDetails(Guid orderId, CancellationToken cancellationToken = default)
+        {
             var orderDetails = await _orderRepository.GetById(orderId, cancellationToken);
             if (orderDetails is null)
             {
-                return Result.NotFound($"Order {orderId} not found");
+                return Result.NotFound<OrderDetails>($"Order {orderId} not found");
             }
             if (orderDetails.Status != OrderStatus.Dispatched)
             {
-                return Result.Failure($"Order {orderId} with status {orderDetails.Status} cannot be set as Delivered");
+                return Result.Failure<OrderDetails>($"Order {orderId} with status {orderDetails.Status} cannot be set as Delivered");
             }
 
+            return orderDetails;
+        }
+
+        private static Result<OrderDetails> ApplyOrderCompletedEvent(OrderDetails orderDetails)
+        {
             orderDetails.Apply(new OrderCompleted());
 
+            return orderDetails;
+        }
+
+        private async Task<Result<Unit>> SaveOrder(OrderDetails orderDetails, CancellationToken cancellationToken = default)
+        {
             await _orderRepository.Save(orderDetails, cancellationToken);
 
             return Result.Success();
